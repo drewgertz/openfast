aca9564ed9523fae93ff9affaca312f45e4459b3
Drew <drew@northwindengineering.com>
2026-06-30 23:33:51 -0400

    Fix CubicSplineInterpM call style for current NWTC_Num interface

--- modules/aerodyn/src/AirfoilInfo_RotCor.f90
+++ modules/aerodyn/src/AirfoilInfo_RotCor.f90
@@ -297,8 +297,8 @@ subroutine AFI_ComputeAirfoilCoefsFromRotCorTable( AOA, RotCorTable, p, AFI_inte
       call MPi2Pi ( Alpha ) ! change AOA into range of -pi to pi
       
       ! Spline interpolation based on requested AOA
-      call CubicSplineInterpM( Alpha, RotCorTable%Alpha, RotCorTable%Coefs, &
-                   RotCorTable%SplineCoefs, IntAFCoefs(1:s1) )
+      IntAFCoefs(1:s1) = CubicSplineInterpM( Alpha, RotCorTable%Alpha, RotCorTable%Coefs, &
+                           RotCorTable%SplineCoefs, ErrStat, ErrMsg )
    end if
   
    AFI_interp%Cl    = IntAFCoefs(p%ColCl)
